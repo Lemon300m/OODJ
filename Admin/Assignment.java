f78c726154fdcdbde8f9c5f1eb4765ac16fdc215
Ka1Jian <166702694+Ka1Jian@users.noreply.github.com>
2024-05-28 10:50:59 +0800

    Add files via upload

---
+++ Admin/Assignment.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author deracho
+ */
+public class Assignment {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
+
+
